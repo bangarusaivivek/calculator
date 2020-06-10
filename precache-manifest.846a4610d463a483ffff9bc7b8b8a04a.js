4cb1eee50c1fea136bbab8d71a79e05cec35c1c9
bangarusaivivek <bangarusaivivek1996@gmail.com>
2020-06-10 20:22:17 +0530

    Updates

---
+++ precache-manifest.846a4610d463a483ffff9bc7b8b8a04a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a0ddcd2ea55e2e5bafa692a30b3168a",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "81526ed25baababe21d4",
+    "url": "/calculator/static/css/main.67c6eb73.chunk.css"
+  },
+  {
+    "revision": "744d2fe832083ab075f0",
+    "url": "/calculator/static/js/2.b4f13e77.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator/static/js/2.b4f13e77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81526ed25baababe21d4",
+    "url": "/calculator/static/js/main.80b07718.chunk.js"
+  },
+  {
+    "revision": "53122c127a7fe401835e",
+    "url": "/calculator/static/js/runtime-main.6de0c573.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/calculator/static/media/logo.5d5d9eef.svg"
+  }
+]);
